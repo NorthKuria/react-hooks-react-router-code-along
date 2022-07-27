fabc0b5e845c4d40ad0f0e2e23447bb20d7849a6
northkuria <kuri4martin@gmail.com>
2022-07-27 09:08:59 +0300

    code along

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function Home() {
   return (
     <div>
       <h1>Home!</h1>
     </div>
-  );
+  )
 }
 
 function App() {
-  return <Home />;
+  return <Home />
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
